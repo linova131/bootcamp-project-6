e82f633bdbd6ac4e32b3dea4b62a11e259dc77ce
Liz Novaski <lnovaski@travelers.com>
2021-02-08 14:57:35 -0600

    JSON Each Iterator
    JSON Each Iterator

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data.json');
-const projects = data.projects
 
 router.get('/', (req, res) => {
-  res.render('index'), projects;
+  res.render('index'), data;
 });
 
 router.get('/', (req, res) => {
