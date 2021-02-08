7c52746fa9e098eeb977d06ccefd0d11caf0a631
Liz Novaski <lnovaski@travelers.com>
2021-02-08 16:13:58 -0600

    testing the JSON parse
    testing the JSON parse

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data.json');
 
-router.get('/', (req, res) => {
-  res.render('index'), data;
+router.get('/', (req,res, next) => {
+  console.log(data.project_name);
+  next();
 });
+
+// router.get('/', (req, res) => {
+//   res.render('index'), data;
+// });
 
 router.get('/', (req, res) => {
   console.log('hello world');
